87fbca36f874c76f7b2f860346f8eeff9a74e2ba
Danila Selvanovich <lineconnor@yandex.ru>
2023-02-27 19:58:37 +0300

    Delete src/main/java/by/it/selvanovich/news/util directory

--- src/main/java/by/it/selvanovich/news/util/validation/UserValidator.java
+++
@@ -1,8 +0,0 @@
-package by.it.selvanovich.news.util.validation;
-
-import jakarta.servlet.http.HttpSession;
-
-public interface UserValidator {
-    boolean haveSession(HttpSession session) throws UserValidatorExeption;
-    boolean isAdmin(String role) throws UserValidatorExeption;
-}
